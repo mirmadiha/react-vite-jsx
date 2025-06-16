3df083aa9864a6b9cc26fbc2b33da71b1786c2da
Madiha Aijaz <mirmadihaaijaz@gmail.com>
2025-06-16 23:50:09 +0530

    creating a component

---
+++ src/App.js
@@ -0,0 +1,9 @@
+//Create a component
+function App(){
+    return(
+        //object cannot be displayed but can be used as a prop
+        <h3><input placeHolder="write your name"/></h3> 
+    )
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,13 @@
 //import the React and ReactDOM library
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App'
 
 //Get a reference to the div with ID root 
 const el=document.getElementById("root");
 
 //Tell React to take control of that element
 const root=ReactDOM.createRoot(el);
-
-//Create a component
-function App(){
-    return(
-        //object cannot be displayed but can be used as a prop
-        <h3><input placeHolder="write your name"/></h3> 
-    )
-}
 
 //Show the component on screen 
 root.render(<App/>);
